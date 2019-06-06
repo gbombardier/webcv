678befda799323d47f391793137115337eb5cda3
Gabriel Bombardier <gbombardier@summit-tech.ca>
2019-06-06 15:47:24 -0400

    Added popup under xp

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -4,4 +4,26 @@ class Utils{
     document.getElementById(fileName+"_link").style.color="black";
     document.getElementById(fileName+"_link").style.borderBottom="1px solid black";
   }
+
+  static putXpText(node, text){
+    switch (text) {
+      case "csmb":
+        node.innerText = "csmb";
+        break;
+      case "summit":
+        node.innerText = "summit";
+        break;
+      case "gcm":
+        node.innerText = "gcm";
+        break;
+      case "makwa":
+        node.innerText = "makwa";
+        break;
+      case "csdgs":
+        node.innerText = "csdgs";
+        break;
+      default:
+          node.innerText = "Pas d'informations";
+    }
+  }
 }
